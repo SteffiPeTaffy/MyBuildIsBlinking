6c2d16c7842e03f43d7c47652a0b7f58f7dae01e
Stefanie Grewenig <sgreweni@thoughtworks.com>
2016-02-13 21:10:45 +0100

    gets cctray xml and parses it

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var express = require('express');
-var app = express();
+var https = require('https');
+var parseString = require('xml2js').parseString;
+var _ = require('lodash');
 
-app.set('port', (process.env.PORT || 5000));
+var app = express();
+var port = process.env.PORT || 5000;
+
+app.set('port', port);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -10,11 +15,44 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
+
+  var getBuildStatusByProjects = function (projects) {
+    var SUCCESS = 'Success';
+
+    var allSucceded = _.every(projects, function(project) {
+      return project.$.lastBuildStatus === SUCCESS;
+    });
+
+    if(allSucceded) {
+      console.log('### LOG',SUCCESS);
+      return SUCCESS;
+    }
+  };
+
+  https.get('https://snap-ci.com/SteffiPeTaffy/conference-rating/branch/master/cctray.xml', (response) => {
+    //console.log('statusCode: ', response.statusCode);
+    //console.log('headers: ', response.headers);
+
+    var completeResponse = '';
+    response.on('data', function (chunk) {
+      completeResponse += chunk;
+    });
+    response.on('end', function() {
+      parseString(completeResponse, function (err, result) {
+        getBuildStatusByProjects(result.Projects.Project);
+      });
+    })
+
+  }).on('error', (e) => {
+    console.error(e);
+  });
+
   response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
 
 
